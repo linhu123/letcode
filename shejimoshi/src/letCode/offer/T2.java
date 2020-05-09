c9f9c8d6d7ec9dfdc615c9c86d5ab9bc624c0599
linhu <13180666@qq.com>
2020-05-09 16:42:22 +0800

    做了offer栏目的第2题

--- shejimoshi/src/letCode/offer/T2.java
+++ shejimoshi/src/letCode/offer/T2.java
@@ -1,4 +1,7 @@
 package letCode.offer;
+
+import java.util.SplittableRandom;
+
 /**
  * 想法 分别对两个字符串进行排序
  * 排完序之后在重新比较两个字符串
@@ -6,19 +9,32 @@ package letCode.offer;
  */
 public class T2 {
     public boolean CheckPermutation(String s1, String s2) {
+        s1 = maopaoSort(s1);
+        s2 = maopaoSort(s2);
+        if (s1.equals(s2))
+            return true;
         return false;
     }
 
     public String maopaoSort(String s){
-        for (int i = 0 ;i<s.length();i++){
-            for (int j = 0;j<s.length();j++){
-//                if (s.charAt(i)>)
+        char[] chars = s.toCharArray();
+        for (int i = chars.length-1 ;i>=0;i--){
+            for (int j = 0;j<i;j++){
+                if (chars[j]>chars[j+1]){
+                    char tmp = chars[j];
+                    chars[j]=chars[j+1];
+                    chars[j+1]=tmp;
+                }
             }
         }
-        return null;
+        return new String(chars);
     }
 
     public static void main(String[] args) {
+        T2 t2 = new T2();
+        String s1 = "asdfg";
+        String s2 = "adsgf";
+        System.out.println(t2.CheckPermutation(s1,s2));
 
     }
 }
